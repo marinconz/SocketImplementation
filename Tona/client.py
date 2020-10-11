f0a0d0ed870c3632955a8ea0102b798b298852c7
Maria Antonia Rincon <marinconz@eafit.edu.co>
2020-10-11 14:47:31 -0500

    Correcciones realizadas al envío de archivos

--- Tona/client.py
+++ Tona/client.py
@@ -1,8 +1,9 @@
 import socket
-import os          
+import os
+import time
    
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+ 
 def main():
   print('Client is running')
   clientSocket.connect(('127.0.0.1', 3000))
@@ -11,10 +12,10 @@ def main():
   print('Enter quit to exit')
   print('Input commands')
   commandToSend = input()
-
+ 
   while commandToSend != 'quit':
     commands = commandToSend.split()
-    
+   
     if(commandToSend == ''):
       print('Please input a valid command')
       commandToSend = input()
@@ -22,15 +23,20 @@ def main():
       if(not os.path.exists(commands[1])):
         print('File does not exist')
       else:
-        clientSocket.send(bytes(commandToSend, 'utf-8'))
         file = open(commands[1], 'rb')
+        length = os.path.getsize(commands[1])
+        print(length)
+        clientSocket.send(bytes(f'{commandToSend} {length}', 'utf-8'))
+        time.sleep(1)
         stream = file.read(1024)
         while(stream):
           clientSocket.send(stream)
           stream = file.read(1024)
         file.close()
+        dataReceived = clientSocket.recv(1024)
+        print(dataReceived.decode('utf-8'))
         commandToSend = input()
-
+ 
     else:
       clientSocket.send(bytes(commandToSend, 'utf-8'))
       dataReceived = clientSocket.recv(1024)
@@ -40,8 +46,8 @@ def main():
   dataReceived = clientSocket.recv(1024)
   print(dataReceived.decode('utf-8'))
   clientSocket.close()
-
-
+ 
+ 
 if __name__ == "__main__":
    # execute only if run as a script
    main()
--- Tona/server.py
+++ Tona/server.py
@@ -1,20 +1,20 @@
 import socket
 import sys
 import os
-
+ 
 DEFAUlT_BUCKET_PATH = './Buckets'
-
+ 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 path = None
 print(str(sys.argv))
-
+ 
 def checkPath(dir):
    if(not os.path.isdir(dir)):
       os.mkdir(dir)
       return('Creacion exitosa')
    else:
       return('El directorio ya existe')
-
+ 
 #Verificar existencia de directorio por argumentos, si es invalido se usa el default
 if(len(sys.argv) > 1):
    if(os.path.isdir(sys.argv[1])):
@@ -27,14 +27,14 @@ else:
    print('No se especifico la ruta del directorio. Se usara una ruta por defecto')
    checkPath(DEFAUlT_BUCKET_PATH)
    path = DEFAUlT_BUCKET_PATH
-
-
+ 
+ 
 # Funcion Main
 def main():
    print("===============================")
    print("Servidor corriendo...")
    create_server_socket()
-
+ 
 # Funcion para iniciar el proceso del servidor
 def create_server_socket():
    tuple_connection = ('127.0.0.1', 3000)
@@ -42,7 +42,7 @@ def create_server_socket():
    server_socket.bind(tuple_connection)
    server_socket.listen(5)
    print('El Socket esta escuchando...', server_socket.getsockname())
-
+ 
    # Ciclo para crear nuevas conexiones
    while True:
       client_connection, client_address = server_socket.accept()
@@ -52,18 +52,23 @@ def create_server_socket():
          remote_string = str(data_received.decode(
             'utf-8'))
          remote_command = remote_string.split()
+         print('comando principal: ', remote_command)
          if(len(remote_command) > 1):
             pathWithParam = f'{path}/{remote_command[1]}'
-        # print('chimbo',remote_command)
          command = remote_command[0]
          print(f'Data recibida de {client_address[0]}:{client_address[1]}')
-
+ 
          #Manejo de variables con y sin parametro extra
          if(command == 'ls' and len(remote_command) == 1):
             response = '\n'.join(os.listdir(path))
             client_connection.sendall(response.encode('utf-8'))
          elif(command == 'ls'):
-            response = ''.join(os.listdir(pathWithParam))
+            try:
+               response = ''.join(os.listdir(pathWithParam))
+            except OSError:
+               response = 'The provided bucket does not exist'
+            if(response == ''):
+               response = 'The selected bucket is empty'
             client_connection.sendall(response.encode('utf-8'))
          elif(command == 'mkbkt'):
             response = checkPath(pathWithParam)
@@ -79,34 +84,34 @@ def create_server_socket():
          elif(command == 'rm' and len(remote_command) == 3):
             if(not os.path.exists(f'{pathWithParam}/{remote_command[2]}')):
                response = 'The file does not exist'
-            else: 
+            else:
                os.remove(f'{pathWithParam}/{remote_command[2]}')
                response = 'File has been deleted successfully'
             client_connection.sendall(response.encode('utf-8'))
          elif(command == 'upload'):
-            print('upload command executed')
-            file = open(f'{path}/{remote_command[2]}/{remote_command[1]}', 'wb')
+            print(remote_command)
+            remotePath = remote_command[1].split('/')
+            fileName = remotePath[len(remotePath) - 1]
+            print(fileName)
+            file = open(f'{path}/{remote_command[2]}/{fileName}', 'wb')
             stream = client_connection.recv(1024)
-            while(stream):
+            while(True):
                file.write(stream)
-               print(not not stream)
-               stream = client_connection.recv(1024)
-               if(stream == b''):
+               if(file.tell() == int(remote_command[3])):
                   break
-            print("closed successfully")
+               stream = client_connection.recv(1024)
             file.close()
             response = 'File transferred successfully'
             client_connection.sendall(response.encode('utf-8'))
-
          elif (command == 'quit'):
             response = 'Connection terminated'
             client_connection.sendall(response.encode('utf-8'))
             break
-
+ 
          
       print(f'Client {client_address[0]}:{client_address[1]} disconnected')
       client_connection.close()
-
+ 
 if __name__ == "__main__":
    # execute only if run as a script
    main()
